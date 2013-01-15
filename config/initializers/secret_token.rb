73aba8203fb39bfb74c42989cd2219ba27c5c4d8
Alex Savin <karismafilms@gmail.com>
2013-01-15 11:48:36 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifbeat::Application.config.secret_token = '8e9b9ee2e69b9a9ab83541ce30609be70964219a9b4ada2cf539521aa1966d1228e9fe7233ff066de5f8a9cc457bd18b3c98d45ddf28a91954ac3ae23f0e2069'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gifbeat::Application.config.session_store :cookie_store, key: '_gifbeat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gifbeat::Application.config.session_store :active_record_store
