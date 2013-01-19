06602d0a6b9fa578bef02e2e79c4802c704eb837
Lari Haataja <lari.haataja@gmail.com>
2013-01-20 00:51:05 +0200

    Earth centered

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
+var geom = {}, models = {};
+
 function webGLStart() {
   var $id = function(d) { return document.getElementById(d); };
-  
+
   //Create moon
-  var earth = new PhiloGL.O3D.Sphere({
+  models.earth = new PhiloGL.O3D.Sphere({
     nlat: 30,
     nlong: 30,
     radius: 2,
@@ -45,46 +47,29 @@ function webGLStart() {
       var gl = app.gl,
           scene = app.scene,
           canvas = app.canvas,
-          //enable specular, color map
-          specularMap = $id('specular-map'),
-          colorMap = $id('color-map'),
-          //get light config from forms
-          lighting = $id('lighting'),
           ambient = {
-            r: $id('ambientR'),
-            g: $id('ambientG'),
-            b: $id('ambientB')
-          },
-          point = {
-            x: $id('lightPositionX'),
-            y: $id('lightPositionY'),
-            z: $id('lightPositionZ'),
-          
-            sr: $id('specularR'),
-            sg: $id('specularG'),
-            sb: $id('specularB'),
-            
-            dr: $id('diffuseR'),
-            dg: $id('diffuseG'),
-            db: $id('diffuseB')
-          },
-          //object rotation
-          theta = 0;
+            r: 0.8,
+            g: 0.8,
+            b: 0.8
+          };
+        //object rotation
+      geom.phi = Math.PI;
+      geom.theta = -Math.PI / 2.0;
 
       //onBeforeRender
-      earth.onBeforeRender = function(program, camera) {
-        program.setUniform('enableSpecularMap', specularMap.checked);
-        program.setUniform('enableColorMap', colorMap.checked);
+      models.earth.onBeforeRender = function(program, camera) {
+        program.setUniform('enableSpecularMap', true);
+        program.setUniform('enableColorMap', true);
       };
       
       //Basic gl setup
-      gl.clearColor(0.0, 0.0, 0.0, 1.0);
+      gl.clearColor(0.0, 0.0, 0.0, 0.0);
       gl.clearDepth(1.0);
       gl.enable(gl.DEPTH_TEST);
       gl.depthFunc(gl.LEQUAL);
       gl.viewport(0, 0, +canvas.width, +canvas.height);
       //Add objects to the scene
-      scene.add(earth);
+      scene.add(models.earth);
       //Animate
       draw();
 
@@ -93,34 +78,17 @@ function webGLStart() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         //Setup lighting
         var lights = scene.config.lights;
-        lights.enable = lighting.checked;
+        lights.enable = true;
         lights.ambient = {
-          r: +ambient.r.value,
-          g: +ambient.g.value,
-          b: +ambient.b.value
-        };
-        lights.points = {
-          diffuse: {
-            r: +point.dr.value,
-            g: +point.dg.value,
-            b: +point.db.value
-          },
-          specular: {
-            r: +point.sr.value,
-            g: +point.sg.value,
-            b: +point.sb.value
-          },
-          position: {
-            x: +point.x.value,
-            y: +point.y.value,
-            z: +point.z.value
-          }
+          r: +ambient.r,
+          g: +ambient.g,
+          b: +ambient.b
         };
         
         //Update position
-        theta += 0.01;
-        earth.rotation.set(Math.PI, theta,  0.1);
-        earth.update();
+        //theta += 0.01;
+        models.earth.rotation.set(geom.phi, geom.theta, 0.0);
+        models.earth.update();
         
         //render objects
         scene.render();
@@ -132,4 +100,61 @@ function webGLStart() {
   });
 }
 
+//center to coordinates
+function centerMap(lat, lng) {
+ var  earth = models.earth,
+      phi = pos[3],
+      theta = pos[4],
+      phiPrev = geom.phi || Math.PI / 2,
+      thetaPrev = geom.theta || (3 * Math.PI / 2),
+      phiDiff = phi - phiPrev,
+      thetaDiff = theta - thetaPrev;
 
+  geom.matEarth = earth.matrix.clone();
+  geom.matCities = cities.matrix.clone();
+
+  fx.start({
+    onCompute: function(delta) {
+      rotateXY(phiDiff * delta, thetaDiff * delta);
+      geom.phi = phiPrev + phiDiff * delta;
+      geom.theta = thetaPrev + thetaDiff * delta;
+    },
+
+    onComplete: function() {
+      centerAirline.app.scene.resetPicking();
+    }
+  });
+}
+
+//rotate the planet of phi and theta angles
+function rotateXY(phi, theta) {
+  var earth = models.earth,
+      cities = models.cities,
+      xVec = [1, 0, 0],
+      yVec = [0, 1, 0],
+      yVec2 =[0, -1, 0];
+
+  earth.matrix = geom.matEarth.clone();
+  cities.matrix = geom.matCities.clone();
+
+  var m1 = new Mat4(),
+      m2 = new Mat4();
+
+  m1.$rotateAxis(phi, xVec);
+  m2.$rotateAxis(phi, xVec);
+
+  m1.$mulMat4(earth.matrix);
+  m2.$mulMat4(cities.matrix);
+
+  var m3 = new Mat4(),
+      m4 = new Mat4();
+
+  m3.$rotateAxis(theta, yVec2);
+  m4.$rotateAxis(theta, yVec);
+
+  m1.$mulMat4(m3);
+  m2.$mulMat4(m4);
+
+  earth.matrix = m1;
+  cities.matrix = m2;
+}
